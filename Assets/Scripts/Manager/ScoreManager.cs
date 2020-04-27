9ead68198e247cdb6c8533c5571a2dfd497a9dc2
Quentin Denois <quentin.denois@hotmail.fr>
2020-04-27 16:02:32 +0200

    Score Pannel

---
+++ Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public enum Team
+{
+    None,
+    Blue,
+    Red
+}
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    [HideInInspector] public int blueTeamScore, redTeamScore;
+
+    [Header("Final Score")] public int FinalScore = 9;
+
+    public TextMeshProUGUI blueTeamScoretxt, redTeamScoretxt;
+
+    public void Goal(Team team)
+    {
+        Team winningTeam = Team.None;
+
+        switch (team)
+        {
+            case Team.Blue:
+                blueTeamScore++;
+                if (blueTeamScore >= FinalScore)
+                    winningTeam = Team.Blue;
+                break;
+
+            case Team.Red:
+                redTeamScore++;
+                if (redTeamScore >= FinalScore)
+                    winningTeam = Team.Red;
+                break;
+        }
+
+        UpdateScorePanel();
+        if(winningTeam != Team.None)
+        {
+            TeamWinTheGame(winningTeam);
+        }
+    }
+
+    public void UpdateScorePanel()
+    {
+        blueTeamScoretxt.text = blueTeamScore.ToString();
+        redTeamScoretxt.text = redTeamScore.ToString();
+    }
+
+    public void TeamWinTheGame(Team winningTeam)
+    {
+        //End the Game
+        //Display winning team screen
+
+        switch (winningTeam)
+        {
+            case Team.Blue:
+                Debug.Log("BLUE TEAM WIN THE GAME !");
+                break;
+
+            case Team.Red:
+                Debug.Log("RED TEAM WIN THE GAME !");
+                break;
+        }
+    }
+}
