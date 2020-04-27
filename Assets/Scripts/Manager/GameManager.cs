f79dde01363fffed98e62b151f13e723b1f9a33f
Rubika-TPiau <t.piau@Rubika-edu.com>
2020-04-27 18:19:13 +0200

    Add GameManager Skeleton

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public event Action<bool> PauseGameEvent;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(this);
+        else
+            instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void SetupGame()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void PauseGame(bool isPaused)
+    {
+        PauseGameEvent?.Invoke(isPaused);
+        // Afficher Menu
+    }
+
+    public void LeaveGame()
+    {
+
+    }
+}
