ebb1730d9520b37d910dce635ae605ed6d7b8c04
Quentin Denois <quentin.denois@hotmail.fr>
2020-04-28 10:15:34 +0200

    Pre-Bumper

---
+++ Assets/Scripts/Gameplay/Bumper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.transform.tag == "Player")
+        {
+            //TriggerBumper(col.GetComponent<Player>());
+        }
+    }
+
+    /*public void TriggerBumper(PlayerPrefs affectedPlayer)
+    {
+        affectedPlayer.Bump();
+    }*/
+
+}
