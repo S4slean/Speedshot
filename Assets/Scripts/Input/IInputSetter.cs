778fc860aa04ee7c4d8c09cb7e528480cfab019b
Rubika-TPiau <t.piau@Rubika-edu.com>
2020-04-30 04:18:28 +0200

    Add Input system

---
+++ Assets/Scripts/Input/IInputSetter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputSetter
+{
+    void SetupInput(int playerID);
+    void UnsetupInput(int playerID);
+}
