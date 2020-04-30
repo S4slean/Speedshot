d9df596db208fab3d5d1f4e6611a8ce14d2ca3e5
Rubika-TPiau <t.piau@Rubika-edu.com>
2020-04-30 11:33:39 +0200

    Add PlayerInputHandler

---
+++ Assets/Scripts/Input/CharacterInputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterInputHandler : MonoBehaviour
+{
+    private PlayerInput _playerInput;
+    private InputAction _verticalAxisAction;
+    private InputAction _horizontalAxisAction;
+    private InputAction _startAction;
+    private InputAction _aButtunAction;
+    private InputAction _bButtunAction;
+
+    public void SetPlayerInput(PlayerInput playerInput)
+    {
+        _playerInput = playerInput;
+
+        _verticalAxisAction = _playerInput.actions["VerticalAxis"];
+        _horizontalAxisAction = _playerInput.actions["HorizontalAxis"];
+        _startAction = _playerInput.actions["Start"];
+        _aButtunAction = _playerInput.actions["A"];
+        _bButtunAction = _playerInput.actions["B"];
+    }
+}
