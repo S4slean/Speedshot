62c549cdff49ea06ac391cb0d44339912bc38ced
Rubika-TPiau <t.piau@Rubika-edu.com>
2020-04-30 22:29:26 +0200

    VersionDebug Operationnel

--- Assets/Scripts/Menu/PlayerFrame.cs
+++ Assets/Scripts/Menu/PlayerFrame.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerFrame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerFrameHandler playerFrameHandler;
+
+    private void Awake()
     {
-        
+        playerFrameHandler = GetComponent<PlayerFrameHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPlayerAssigned()
     {
-        
+
+    }
+
+    public void OnPlayerUnassigned()
+    {
+
     }
 }
