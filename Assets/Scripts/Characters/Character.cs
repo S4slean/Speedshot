4e224695e8476bc6ec452577616abdfe185003cd
Stanislas Dubuc <stanislas.dubuc@gmail.com>
2020-04-30 16:51:27 +0200

    Sound on character

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -546,6 +546,7 @@ public class Character : MonoBehaviour
 		jumping = true;
 		jumpTracker = 0;
 		anim.Play("Jump");
+		AudioManager2D.instance.PlaySound("Player_Jump", transform.position);
 	}
 
 	public void WallJump()
@@ -553,6 +554,7 @@ public class Character : MonoBehaviour
 		jumping = false;
 		wallJumping = true;
 		wallJumpTracker = 0;
+		AudioManager2D.instance.PlaySound("Player_Jump", transform.position);
 
 		if (wallRide == WallRide.Right)
 			wallJumpDir = -1;
@@ -595,6 +597,7 @@ public class Character : MonoBehaviour
 		attack = Attack.AirDash;
 		anim.Play("AirDash");
 		attackDir = movementAxis.x != 0 ? Mathf.Sign(movementAxis.x) : dir;
+		AudioManager2D.instance.PlaySound("Player_Dash", transform.position);
 		//if (wallRide == WallRide.Right) attackDir = -1;
 		//else if (wallRide == WallRide.Left) attackDir = 1;
 
@@ -609,6 +612,7 @@ public class Character : MonoBehaviour
 		slideTracker = 0;
 		attack = Attack.Slide;
 		anim.Play("Slide");
+		AudioManager2D.instance.PlaySound("Player_Dash", transform.position);
 		attackDir = movementAxis.x != 0 ? Mathf.Sign(movementAxis.x) : dir;
 	}
 
@@ -641,6 +645,7 @@ public class Character : MonoBehaviour
 
 	public void Bump(Bumper bumper)
 	{
+		
 		triggeredBumper = bumper;
 		triggeredBumper.bumpTracker = 0f;
 		bumping = true;
@@ -653,11 +658,11 @@ public class Character : MonoBehaviour
 		anim.SetBool("wallSliding", wallRide == WallRide.None ? false : true);
 
 
-		if (wallRide == WallRide.Right)
+		if (wallRide == WallRide.Right && !grounded)
 		{
 			transform.localScale = new Vector3(-1, 1, 1);
 		}
-		else if (wallRide == WallRide.Left)
+		else if (wallRide == WallRide.Left && !grounded)
 		{
 			transform.localScale = new Vector3(1, 1, 1);
 		}
