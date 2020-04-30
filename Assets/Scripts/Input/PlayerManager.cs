8471f9d22f73e23c3dcfe9d36dd5a64e9ab432d5
Rubika-TPiau <t.piau@Rubika-edu.com>
2020-04-30 20:16:08 +0200

    Debug mod MainMenu

--- Assets/Scripts/Input/PlayerManager.cs
+++ Assets/Scripts/Input/PlayerManager.cs
@@ -10,6 +10,9 @@ public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager instance;
 
+    public bool debugMode;
+    private TeamEnum previousValue = TeamEnum.TEAM1;
+
     [HideInInspector] public Player[] players = new Player[4];
 
 
@@ -49,7 +52,13 @@ public class PlayerManager : MonoBehaviour
         {
             if(players[i] == null)
             {
-                players[i] = new Player(i, TeamEnum.NONE, newPlayer);
+                if(debugMode)
+                {
+                    previousValue = (TeamEnum)((int)previousValue % 2 + 1);
+                    players[i] = new Player(i, previousValue, newPlayer);
+                }
+                else
+                    players[i] = new Player(i, TeamEnum.NONE, newPlayer);
 
                 newPlayer.gameObject.transform.parent = transform;
                 newPlayer.gameObject.name = "Player " + (i + 1);
--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -5,15 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public static MainMenuManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+            instance = this;
+    }
+
     private void Start()
     {
         
     }
 
-
-
     private void LaunchGame()
     {
 
+    }
+    
+    public void DebugLaunch()
+    {
+        SceneManager.LoadScene("Test - Quentin 2");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DebugLaunch();
+        }
     }
 }
